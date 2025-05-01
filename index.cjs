1f7276f5f16dcf91e642a14d8e737d3df10d0e0c
dave <78284066+davidhbt@users.noreply.github.com>
2025-05-02 00:53:06 +0300

    Update index.cjs ss

--- index.cjs
+++ index.cjs
@@ -14,7 +14,7 @@ app.use('/callback', bodyParser.json());  // For handling raw JSON from Chapa's 
 
 // 1. Checkout endpoint (User initiates payment)
 app.post('/api/checkout', async (req, res) => {
-  const { amount, email, first_name, last_name, phone_number, currency = 'ETB', return_url } = req.body;
+  const { amount, email, first_name, last_name, phone_number, currency = 'ETB' } = req.body;
   const tx_ref = `tx-${Date.now()}`;  // Unique transaction reference
 
   try {
@@ -26,9 +26,8 @@ app.post('/api/checkout', async (req, res) => {
       last_name,
       phone_number,
       tx_ref,
-      // callback_url, // Your callback URL
       callback_url: 'http://localhost:5400/callback',  // Your callback URL
-      return_url,
+      // return_url: 'http://localhost:5400/thank-you',  // URL to redirect user after payment
       customizations: {
         title: 'mahi market',
         description: 'Payment for your awesome service'
